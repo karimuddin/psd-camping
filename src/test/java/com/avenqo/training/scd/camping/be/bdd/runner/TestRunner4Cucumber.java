f076a6690d6cc7c8cc356240630f5a5785b746a7
hko <hko@avenqo.com>
2020-10-07 06:48:50 +0200

    Cucumber Reporting -> JSON

--- src/test/java/com/avenqo/training/scd/camping/be/bdd/runner/TestRunner4Cucumber.java
+++ src/test/java/com/avenqo/training/scd/camping/be/bdd/runner/TestRunner4Cucumber.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "html:target/results.html", "message:target/results.ndjson" }, 
+@CucumberOptions(plugin = { "html:target/reports/cucumber-results.html", "json:target/reports/cucumber-results.json", "message:target/reports/cucumber-results.ndjson" }, 
 	features = {"src/test/resources/Features/attd/" },
 	glue = {"com.avenqo.training.scd.camping.be.bdd.steps"}
 )
