338227637e6398dd3bad4d22e67894eb7001b203
Peter Gugel <peter.gugel@gft.com>
2020-10-07 11:19:20 +0200

    Behebungen von Warnungen

--- src/main/java/com/avenqo/training/scd/camping/be/model/CampSiteModel.java
+++ src/main/java/com/avenqo/training/scd/camping/be/model/CampSiteModel.java
@@ -1,63 +1,41 @@
 package com.avenqo.training.scd.camping.be.model;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import com.avenqo.training.scd.camping.be.dao.DaoConsistencyException;
 import com.avenqo.training.scd.camping.be.dao.SiteDao;
 import com.avenqo.training.scd.camping.be.entities.Company;
-import com.avenqo.training.scd.camping.be.entities.InvalidDataException;
 import com.avenqo.training.scd.camping.be.entities.Site;
+
+import java.util.logging.Logger;
 
 public class CampSiteModel {
 
-	// ----------- Constants ------------
+    // ----------- Constants ------------
+    final private static Logger LOG = Logger.getLogger(CampSiteModel.class.getName());
 
-	final private static Logger LOG = Logger.getLogger(CampSiteModel.class.getName());
+    // ----------- Attributes ------------
+    private Company company;
+    private SiteDao siteDao = new SiteDao();
 
-	// ----------- Attributes ------------
+    // ----------- Construction ------------
+    public CampSiteModel(SiteDataModel sitesDataModel) throws DaoConsistencyException {
+        // Init Company
+        company = new Company("Schöner Blick GmbH & Co. KG, 98765 Pimpelhaus, Am Weinberg 42", "DE12345678");
+        if (sitesDataModel != null) {
+            siteDao.addSites(sitesDataModel);
+        }
+    }
 
-	private Company company = null;
-	private SiteDao siteDao = new SiteDao();
+    // ----------- Methods ------------
+    public Integer getNumberOfPlaces() {
+        return siteDao.getAllSites().size();
+    }
 
-	// ----------- Methods ------------
+    public Company getCompany() {
+        return new Company(company);
+    }
 
-	public Integer getNumberOfPlaces() {
-		return siteDao.getAllSites().size();
-	}
-
-	public Company getCompany() {
-		return new Company(company);
-	}
-	
-	public Site get(String id) {
-		return siteDao.getSite(id);
-	}
-
-	// ----------- Construction ------------
-
-	private static CampSiteModel instance = null;
-
-	static {
-		try {
-			instance = new CampSiteModel(new SiteDataModel());
-		} catch (DaoConsistencyException | InvalidDataException e) {
-			LOG.log(Level.SEVERE, "CampSiteModel isn't successfully initialized!", e);
-		}
-	}
-
-	public CampSiteModel(SiteDataModel sitesDataModel) throws DaoConsistencyException, InvalidDataException {
-		// Init Company
-		company = new Company("Schöner Blick GmbH & Co. KG, 98765 Pimpelhaus, Am Weinberg 42", "DE12345678");
-		if (sitesDataModel != null ) {
-			siteDao.addSites(sitesDataModel);
-		}
-	}
-
-//	public static CampSiteModel getInstance() throws InvalidDataException, DaoConsistencyException {
-//		return instance;
-//	}
-
-	
+    public Site get(String id) {
+        return siteDao.getSite(id);
+    }
 
 }
